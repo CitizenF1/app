54694f026d1679e0c513ad877f1eeeb85406a6c6
Azamat <AXpert22@gmail.com>
2020-12-24 12:20:06 +0600

    configdb

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db');
 
 const app = express();
 
 const port = 3000;
 
+mongoose.connect(config.db);
+
 app.listen(port, () => {
-    console.log("Сервер был запущен на порту: " + port)
+    console.log("Server was running on the port: " + port)
 });
 
 app.get('/', (req, res) => {
-    res.send("Главная страница сайта!")
+    res.send("Home page!")
 });
-ss
