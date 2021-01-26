614336ab86191998b27d782d76da7861d17f9187
Azamat <AXpert22@gmail.com>
2021-01-26 19:39:22 +0600

    11

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FlashMessagesModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/reg/reg.component.ts
+++ front-end/src/app/reg/reg.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-reg',
@@ -12,13 +13,40 @@ export class RegComponent implements OnInit {
   email: string | undefined
   password: string | undefined
 
-  constructor() { }
+  constructor(private _flashMessagesService: FlashMessagesService)
+    
+ { }
 
   ngOnInit(): void {
   }
 
-  signUp(){
-    console.log("work")
+  signUp() {
+    const user = {
+      name: this.name,
+      login: this.login,
+      email: this.email,
+      password: this.password,
+    }
+    if(!user.name){
+      this._flashMessagesService.show('Enter your name!', 
+      { cssClass: 'alert-danger', timeout: 3000 });
+      return false
+    }
+    else if(!user.login){
+      this._flashMessagesService.show('Enter your login!', 
+      { cssClass: 'alert-danger', timeout: 3000 });
+      return false
+    }
+    else if(!user.email){
+      this._flashMessagesService.show('Enter your email!', 
+      { cssClass: 'alert-danger', timeout: 3000 });
+      return false
+    }
+    else if(!user.password){
+      this._flashMessagesService.show('Enter your password!', 
+      { cssClass: 'alert-danger', timeout: 3000 });
+      return false
+    }
     return false
   }
 
